57003dbc68156585ffa034046b7eff611811b31b
rinater2002 <134974176+rinater2002@users.noreply.github.com>
2024-06-05 23:44:39 +0300

    Delete insertTablesW.sql

--- insertTablesW.sql
+++
@@ -1,4 +0,0 @@
-select * from Workers;
-INSERT INTO Workers VALUES ('Manager', 7500.50, 'manager1@example.com', '123-456-7890', 1, 101, 'John Doe');
-INSERT INTO Workers VALUES ('Engineer', 6500.75, 'engineer2@example.com', '234-567-8901', 2, 102, 'Jane Smith');
-INSERT INTO Workers VALUES ('Technician', 5500.00, 'tech3@example.com', '345-678-9012', 3, 103, 'Bob Johnson');
