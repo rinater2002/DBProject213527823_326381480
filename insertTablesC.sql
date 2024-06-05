c118fa16899a7e82e62afc5ab799c5721a7856db
rinater2002 <134974176+rinater2002@users.noreply.github.com>
2024-06-05 23:44:16 +0300

    Delete insertTablesC.sql

--- insertTablesC.sql
+++
@@ -1,4 +0,0 @@
-select * from Customers;
-INSERT INTO Customers VALUES ('Alice Johnson', 301, '123-456-7890', 'Regular', 'alice.johnson@example.com');
-INSERT INTO Customers VALUES ('Bob Smith', 302, '234-567-8901', 'Premium', 'bob.smith@example.com');
-INSERT INTO Customers VALUES ('Carol Williams', 303, '345-678-9012', 'Business', 'carol.williams@example.com');
